d4a3a51accb879c48451551ee16258e372f90fa9
asmaaelhadad <asmaa.elhadad@gmail.com>
2023-02-10 13:06:37 +0100

    signUp

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,5 +5,9 @@ const router = express.Router();
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+router.get("/signup", (req, res, next) => {
+  res.render("signUp");
+});
+
 
 module.exports = router;
